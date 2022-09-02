30fe5dc913be76ae139c1d64aa7115fde1c08a6b
risusan87 <risusan8795@gmail.com>
2022-09-02 13:27:51 -0400

    Package entity updated

--- src/jp/risu87/hzp/entity/ai/AILookAtFarPlayer.java
+++ src/jp/risu87/hzp/entity/ai/AILookAtFarPlayer.java
@@ -1,69 +1,55 @@
 package jp.risu87.hzp.entity.ai;
 
 import net.minecraft.server.v1_12_R1.EntityInsentient;
-import net.minecraft.server.v1_12_R1.EntityLiving;
+import net.minecraft.server.v1_12_R1.EntityPlayer;
 import net.minecraft.server.v1_12_R1.IEntitySelector;
-import net.minecraft.server.v1_12_R1.PathfinderGoal;
+import net.minecraft.server.v1_12_R1.World;
 
 import com.google.common.base.Predicates;
 
-public class AILookAtFarPlayer extends PathfinderGoal {
-	
-	protected EntityInsentient looker;
+public final class AILookAtFarPlayer extends AIBase {
 
-	protected EntityLiving b;
-
-	protected float c;
+	private EntityPlayer target;
+	private final float range;
 
 	public AILookAtFarPlayer(EntityInsentient aiHolder) {
-		this.looker = aiHolder;
-		this.c = 256.f;
+		super(aiHolder);
+		this.range = 256.f;
 		a(0);
 	}
 	
-	/**
-	 * shouldAIActivated
-	 */
-	public boolean a() {
-		if (this.looker.getGoalTarget() == null) {
-			this.b = this.looker.world.a(this.looker.locX, this.looker.locY, this.looker.locZ, this.c, Predicates.and(IEntitySelector.e, IEntitySelector.b(this.looker)));
-			this.looker.setGoalTarget(this.b);
+	@Override
+	public boolean isAIApplicable() {
+		if (this.holder.getGoalTarget() == null) {
+			World world = this.holder.world;
+			double x = holder.locX, y = holder.locY, z = holder.locZ;
+			this.target = (EntityPlayer) world.a(x, y, z, this.range, Predicates.and(IEntitySelector.e, IEntitySelector.b(this.holder)));
+			this.holder.setGoalTarget(this.target);
 		} else {
-			this.b = this.looker.getGoalTarget();
+			this.target = (EntityPlayer) holder.getGoalTarget();
 		}
 		
 		return true;
 	}
 	
-	/**
-	 * shouldAIProcessContinue
-	 */
-	public boolean b() {
-		if (!this.b.isAlive())
+	@Override
+	public boolean shouldContinue() {
+		if (!this.target.isAlive())
 			return false; 
-		if (this.looker.h(this.b) > (this.c * this.c))
+		if (this.holder.h(this.target) > (range * range))
 			return false;
 		else 
 			return true;
 	}
 	
-	/**
-	 * onAIActivate
-	 */
-	public void c() {
+	@Override
+	public void onFinalize() {
+		this.target = null;
 	}
 	
-	/**
-	 * onAIDeactivate
-	 */
-	public void d() {
-		this.b = null;
-	}
-	
-	/**
-	 * onAIUpdate
-	 */
-	public void e() {
-		this.looker.getControllerLook().a(this.b.locX, this.b.locY + this.b.getHeadHeight(), this.b.locZ, 10, 40);
+	@Override
+	public void onUpdate() {
+		double x = target.locX, y = target.locX + target.getHeadHeight(), z = target.locX;
+		this.holder.getControllerLook().a(x, y, z, 10, 40);
 	}
 }
